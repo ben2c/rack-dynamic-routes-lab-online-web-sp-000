a997a52fed980febff6fe4a48027fcba822854a1
Benjamin Cheng <bencheng200@gmail.com>
2020-12-22 19:31:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,13 @@ class Application
     resp = Rack:Response.new
     req = Rack::Request.new
 
+    if req.path == "/songs"
+      resq.write "You've request the songs"
+    else 
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    
+    resp.finish
   end
-
 end
